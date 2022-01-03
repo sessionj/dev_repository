b1a47d5dfbd5e7a41cfbe614c61a32e7af19b3e1
sessionj <_session@kakao.com>
2022-01-03 15:48:33 +0900

    project 202201031548

--- UtilityJava/src/kr/co/ds/domain/UnsongEntity.java
+++ UtilityJava/src/kr/co/ds/domain/UnsongEntity.java
@@ -3,12 +3,19 @@ package kr.co.ds.domain;
 public class UnsongEntity {
 
 	private String billno;
+	private String serialno;
 
 	public String getBillno() {
 		return billno;
 	}
 	public void setBillno(String billno) {
 		this.billno = billno;
+	}
+	public String getSerialno() {
+		return serialno;
+	}
+	public void setSerialno(String serialno) {
+		this.serialno = serialno;
 	}
 	
 }
